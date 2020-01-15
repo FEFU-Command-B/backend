a98c0846da4930af62b03da1233f91d2b8c622f0
micha <micha030201@gmail.com>
2020-01-15 19:00:10 +1000

    mock route

--- vladik.py
+++ vladik.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 
-app = Flask('yarmarka')
+app = Flask('vladik')
 
 
 @app.route('/question', methods=['GET', 'POST'])
@@ -15,6 +15,28 @@ def question():
             '18-30',
             '30-50',
             '50-160',
+        ],
+    })
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+@app.route('/route', methods=['GET', 'POST'])
+def route():
+    resp = jsonify({
+        'route': [
+            {
+                'name': 'Прогулка по набережной',
+                'location': 'на набережной',
+                'time': '10:00-11:00',
+                'description': 'lorem ipsum',
+            },
+            {
+                'name': 'Завтрак в кафе',
+                'location': 'какое-нибудь кафе (придумать)',
+                'time': '11:00-13:00',
+                'description': 'lorem ipsum',
+            },
         ],
     })
     resp.headers['Access-Control-Allow-Origin'] = '*'
