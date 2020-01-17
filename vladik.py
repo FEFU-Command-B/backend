f0decc330d5ff0330f576ff7738f4097e8acee87
Max <max3jk@gmail.com>
2020-01-17 20:49:32 +1000

    add id

--- vladik.py
+++ vladik.py
@@ -1,26 +1,39 @@
 from flask import Flask, jsonify
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+import urllib
 import os
 
-app = Flask('vladik', static_url_path='/static')
-
-#engine = create_engine(os.environ['constr'], echo=True)
-
+#params = urllib.parse.quote_plus(os.environ['constr2'])
+#conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+#engine = create_engine(conn_str, echo=True)
 #meta = MetaData()
 
+#print('connection is ok')
+
+#print(os.environ['constr'])
+
 #students = Table(
- #   'places', meta,
+##    'places', meta,
 #    Column('id', Integer, primary_key=True),
 #    Column('name', String),
 #    Column('description', String),
 #)
 #meta.create_all(engine)
 
+q = """
+SELECT count(*) FROM places
+"""
+#res = engine.connect().execute(q)
+
+#print(res)
+
+app = Flask('vladik', static_url_path='/static')
 
 
 @app.route('/question', methods=['GET', 'POST'])
 def question():
     resp = jsonify({
+        'id': 0,
         'question': (
             'Здравствуй! Я Владик, давай познакомимся!'
             ' Мне 160 лет, а тебе?'
@@ -40,6 +53,7 @@ def question():
 def question_answer(option):
     if option in ['0-17', '18-30', '30-50', '50-160']:
         resp = jsonify({
+            'id': 1,
             'question': (
                 'Расскажи о цели своего визита, мне очень хочется,'
                 ' чтобы я тебе понравился!'
